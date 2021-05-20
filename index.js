90874e9b4459a9d9619ef3022f66844f004309d2
Maksym Sharin <Maksym.sharin.work@gmail.com>
2021-05-20 16:53:46 +0200

    feat:  buttons

--- index.js
+++ index.js
@@ -1,32 +1,44 @@
 window.onload = () => {
-    const fetchProducts = async function( obj ){
+    const fetchProducts = async function (obj) {
         await fetch('https://striveschool-api.herokuapp.com/books')
-            .then( response => response.json() )
-            .then( data => obj['productFetchData'] = data )
+            .then(response => response.json())
+            .then(data => obj['productFetchData'] = data)
     }
-    const renderProducts = function ( prodsRawArr = [] ){
-        prodsRawArr.forEach(
-            (el,i,a)=>{
-            let prodCard = document.createElement('div')
-                $('#productsListing .row')[0].append(prodCard)
-                prodCard.outerHTML =
-                    `<div class="col mb-4">
-                    <div class="card">
-                    <img src=" ${ el['img'] } " class="card-img-top">
-                    <div class="card-body">
-                        <h5 class="card-title">
-                            ${ el['title'] }
-                        </h5>
-                    </div>
-                    </div>
-                </div>`
-            }
-        )
-    }
-    ;(async function Main(){
+    const renderProducts = function (prodsRawArr = []) {
+            prodsRawArr.forEach(
+                (el, i, a) => {
+                    let prodCard = document.createElement('div')
+                    $('#productsListing .row')[0].append(prodCard)
+                    prodCard.outerHTML = `
+                    <div class="col mb-4">
+                        <div class="card">
+                        <img src=" ${el['img']} " class="card-img-top">
+                        <div class="card-body">
+                            <h5 class="card-title">
+                            ${el['title']}
+                            </h5>
+                            <div class="d-flex">
+                                <button class="btn btn-primary" type="submit">Add to Card</button>
+                                <button class="btn btn-primary" type="submit">Skip</button>
+                                <button class="btn btn-primary" type="submit">Ignore</button>
+                                <button class="btn btn-primary" type="submit">Delete</button>
+                            </div>
+                            </div>
+                        </div>
+                    </div>`
+                    prodCard.querySelectorAll('btn')[0].addEventListener('click')
+                    prodCard.querySelectorAll('btn')[1].addEventListener('click')
+                    prodCard.querySelectorAll('btn')[2].addEventListener('click')
+                    prodCard.querySelectorAll('btn')[3].addEventListener('click')
+
+                }
+            )
+        }
+    ;(async function Main() {
         let dataObj = {};
-        await fetchProducts( dataObj ); console.log( dataObj );
-        renderProducts( dataObj.productFetchData );
+        await fetchProducts(dataObj);
+        console.log(dataObj);
+        renderProducts(dataObj.productFetchData);
     })()
 }
 
